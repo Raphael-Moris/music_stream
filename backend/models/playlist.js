7d45115751395e1dc8e8c4898f8428771c38a08c
MORISCOT Raphael <44001405@co.univ-reunion.fr>
2025-10-19 14:38:24 +0400

    Delete backend directory

--- backend/models/playlist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const playlistSchema = new mongoose.Schema({
-  name: String,
-  genre: String,
-  songs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song' }]
-});
-
-module.exports = mongoose.model('Playlist', playlistSchema);
